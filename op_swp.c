30cada92d7a8ea63a0c6c88e2d6d86aba68465a6
N <51336368+Nmcleon@users.noreply.github.com>
2023-08-25 01:27:56 +0200

    Create op_swp.c

---
+++ op_swp.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+/**
+ * op_swap - function that swaps the two values at the top of stack.
+ * @stack: pointer to  top of a stack list.
+ * @number: line number of a Monty bytecodes file.
+ */
+void op_swap(stack_t **stack, unsigned int op_number)
+{
+	int temp;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%i: can't swap, stack too short\n", op_number);
+		exit(EXIT_FAILURE);
+	}
+  
+	temp = (*stack)->next->n;
+	(*stack)->next->n = (*stack)->n;
+	(*stack)->n = temp;
+}
