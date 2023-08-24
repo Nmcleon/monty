ad4a6c2ccbbb686fa237d681ae871b1e802d541f
N <51336368+Nmcleon@users.noreply.github.com>
2023-08-25 01:41:34 +0200

    Create op_sub.c

---
+++ op_sub.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * op_sub - A function that subtracts the second value from the top of a
+ * stack: linked by the top value.
+ * @stack: pointer to the stack list.
+ * @number: line number of a Monty bytecodes file.
+ * Description: result is stored in second node
+ *              from top and the top value removed.
+ */
+void op_sub(stack_t **stack, unsigned int number)
+{
+	int temp;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%i: can't sub, stack too short\n", number);
+		exit(EXIT_FAILURE);
+	}
+	temp = (*stack)->next->n - (*stack)->n;
+	(*stack)->next->n = temp;
+	monty_pop(stack, number);
+}
