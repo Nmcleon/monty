800816d34a4503bbcc11551933c77d84d32a856e
N <51336368+Nmcleon@users.noreply.github.com>
2023-08-25 01:18:17 +0200

    Update op_pop.c

--- op_pop.c
+++ op_pop.c
@@ -6,16 +6,17 @@
  */
 void op_pop(stack_t **stack, unsigned int number)
 {
-	stack_t *temp_s = *stack;
-
-	if (!temp_s)
+	stack_t *temp;
+	
+	if (*stack == NULL)
 	{
-		f_errors(2, number);
+		fprintf(stderr, "L%i: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
-	if (temp_s->next)
-		temp_s->next->prev = temp_s->prev;
-	*stack = temp_s->next;
-	free(temp_s);
+	temp = *stack;
+	*stack = (*stack)->next;
+	if (*stack)
+		(*stack)->prev = NULL;
+	free(temp);
 }
