fed22325a7ff2b96fdac43e4c7e52ccd77602ee0
N <51336368+Nmcleon@users.noreply.github.com>
2023-08-24 22:10:16 +0200

    Update monty.h

--- monty.h
+++ monty.h
@@ -38,7 +38,6 @@ void free_s(stack_t **s);
 char **my_token(char *c, char *delim);
 int bytecode_run(FILE *file);
 int execute(char **token, stack_t **stack, unsigned int num);
-int get_line(char **line, size_t *len, FILE *file);
 
 /**
  * struct instruction_s - opcode and its function
