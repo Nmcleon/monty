f9ee2309db7e16e8c5f1cdbecb63233d4e9244f7
N <51336368+Nmcleon@users.noreply.github.com>
2023-08-25 01:58:37 +0200

    Create op_pchar.c

---
+++ op_pchar.c
@@ -0,0 +1,27 @@
+#include "monty.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * op_pchar - function that prints char at top value of the stack.
+ * @stack: pointer to the stack list.
+ * @number: current working line number of a Monty bytecodes file.
+ */
+void op_pchar(stack_t **stack, unsigned int number)
+{
+	int number = 0;
+	stack_t *tmp = *stack;
+
+	if (!tmp)
+	{
+		fprintf(stderr, "L%i: can't pchar, stack empty\n", number);
+		exit(EXIT_FAILURE);
+	}
+	number = tmp->n;
+	if (number < 0 || number > 127)
+	{
+		fprintf(stderr, "L%i: can't pchar, value out of range\n", number);
+		exit(EXIT_FAILURE);
+	}
+	printf("%c\n", tmp->n);
+}
