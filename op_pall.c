7d2dcf44883ec7188aeafb150a0c0cb09263dfe3
Nmcleon <nmcleon10@gmail.com>
2023-08-24 09:54:56 -0700

    betty

--- op_pall.c
+++ op_pall.c
@@ -4,19 +4,18 @@
 #include <stdlib.h>
 
 /**
- * pall - prints the values of the stack.
- * @stack: pointer to the top of a stack.
- * @line_num: number of the line.
- * Return: Nothing.
- */
+* pall - prints value of the stack.
+* @stack: pointer to the top of a stack.
+* @num: number of the line.
+* Return: Nothing.
+*/
 void pall(stack_t **stack, unsigned int num)
 {
-	stack_t *temp_stack = *stack;
-	(void)num;
-
-	while (temp_stack != NULL)
+stack_t *temp_stack = *stack;
+(void)num;
+while (temp_stack != NULL)
 	{
-		printf("%d\n", temp_stack->n);
-		temp_stack = temp_stack->next;
+printf("%d\n", temp_stack->n);
+temp_stack = temp_stack->next;
 	}
 }
