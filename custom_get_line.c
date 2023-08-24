34125efb9862e4e383255dd7eef27b171938868f
Nmcleon <nmcleon10@gmail.com>
2023-08-24 15:03:06 -0700

    betty

--- custom_get_line.c
+++ custom_get_line.c
@@ -1,23 +1,24 @@
 #include "monty.h"
 
 /**
- * custom_get_line - Custom version of getline
- * @line: Pointer to the line buffer.
- * @len: Pointer to the buffer size.
- * @file: File pointer.
- * Return: Number of characters read, or -1 on failure.
- */
+* custom_get_line - Custom version of getline
+* @line: Pointer to the line buffer.
+* @len: Pointer to the buffer size.
+* @file: File pointer.
+* Return: Number of characters read, or -1 on failure.
+*/
 int custom_get_line(char **line, size_t *len, FILE *file)
 {
-    ssize_t read = getline(line, len, file);
-    if (read == -1)
-    {
-        if (feof(file))
-        {
-            return (-1);
-        }
-        perror("Error reading line");
-        exit(EXIT_FAILURE);
-    }
-    return (read);
+	ssize_t read = getline(line, len, file);
+
+	if (read == -1)
+	{
+		if (feof(file))
+		{
+			return (-1);
+		}
+		perror("Error reading line");
+		exit(EXIT_FAILURE);
+	}
+	return (read);
 }
