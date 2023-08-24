e2c021559a81b111090b32680da3c905552db414
N <51336368+Nmcleon@users.noreply.github.com>
2023-08-24 16:55:51 +0200

    Update op_pall

--- op_pall.c
+++ op_pall.c
@@ -4,25 +4,6 @@
 #include <stdlib.h>
 
 /**
- * pall - Prints all the values on the stack.
- * @stack: Double pointer to the stack's head.
- * @line_number: Line number in the Monty byte code file.
- */
-void pall(stack_t **stack, unsigned int line_number)
-{
-	if (stack_is_empty(*stack))
-    {
-        fprintf(stderr, "pall: stack empty\n");
-        exit(EXIT_FAILURE);
-    }
-    else
-    {
-        stack_t *tmp = stack_pop(stack);
-        pall(stack, line_number);
-        stack_push(stack, tmp);
-    }
-}
-/**
  * pall - prints the values of the stack.
  * @stack: pointer to the top of a stack.
  * @line_num: number of the line.
