786b0823efff29fe7c673d524dad59cddc961423
N <51336368+Nmcleon@users.noreply.github.com>
2023-08-25 00:57:14 +0200

    Update op_pint.c

--- op_pint.c
+++ op_pint.c
@@ -1,12 +1,12 @@
 #include "monty.h"
 
 /**
- * pint - function prints the top value of the stack.
+ * op_pint - function prints the top value of the stack.
  * @stack: pointer to the stack list.
  * @line_number: working line number of a Monty bytecodes file. 
  */
 
-void pint(stack_t **stack, unsigned int number)
+void op_pint(stack_t **stack, unsigned int number)
 {
 	stack_t *temp_s = *stack;
 
