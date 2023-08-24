e305f8f97995b13797b973f61fbd0f2c8b493d20
Nmcleon <nmcleon10@gmail.com>
2023-08-24 09:59:35 -0700

    betty

--- op_push.c
+++ op_push.c
@@ -4,51 +4,46 @@
 #include <stdlib.h>
 
 /**
- * op_push - Push element to stack.
- * @stack: pointer to the stack
- * @num: number in the Monty byte file.
- * Return: EXIT_SUCCESS
- */
+* op_push - Push element to stack.
+* @stack: pointer to the stack
+* @num: number in the Monty byte file.
+* @token: pointes to command
+* Return: EXIT_SUCCESS
+*/
 int op_push(stack_t **stack, char **token, unsigned int num)
 {
-    stack_t *n_stack;
-	int i = 0;
-
-	if (token[1] == NULL)
-		return (error(0, num));
-		
-	while (token[1][i])
+stack_t *n_stack;
+int i = 0;
+if (token[1] == NULL)
+	return (error(0, num));
+while (token[1][i])
 	{
-		if (token[1][i] == '-' && i == 0)
+if (token[1][i] == '-' && i == 0)
 		{
-			i++;
-			continue;
+i++;
+continue;
 		}
-		if (token[1][i] < '0' || token[1][i] > '9')
+if (token[1][i] < '0' || token[1][i] > '9')
 		{
-			free_s(stack);
-			return (error(0, num));
+free_s(stack);
+return (error(0, num));
 		}
-		i++;
+i++;
 	}
-
-	n_stack = malloc(sizeof(stack_t));
-	if (n_stack == NULL)
-		return (error_1(0));
-
-	n_stack->n = atoi(token[1]);
-	n_stack->prev = NULL;
-	
-	if ((*stack) != NULL)
+n_stack = malloc(sizeof(stack_t));
+if (n_stack == NULL)
+	return (error_1(0));
+n_stack->n = atoi(token[1]);
+n_stack->prev = NULL;
+if ((*stack) != NULL)
 	{
-		(*stack)->prev = n_stack;
-		n_stack->next = *stack;
+(*stack)->prev = n_stack;
+n_stack->next = *stack;
 	}
-	else
+else
 	{
-		n_stack->next = NULL;
+n_stack->next = NULL;
 	}
-	
-	*stack = n_stack;
-	return (EXIT_SUCCESS);
+*stack = n_stack;
+return (EXIT_SUCCESS);
 }
