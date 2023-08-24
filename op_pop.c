0cf7690396c02012b21a296e4590152399dbf001
Nmcleon <nmcleon10@gmail.com>
2023-08-24 16:21:27 -0700

    op_pop

--- op_pop.c
+++ op_pop.c
@@ -1,22 +1,19 @@
 #include "monty.h"
 /**
- * op_pop - function removes top value of a stack.
- * @stack: pointers to stack_t list.
- * @number: line number of Monty bytecodes file.
- */
+* op_pop - function removes top value of a stack.
+* @stack: pointers to stack_t list.
+* @number: line number of Monty bytecodes file.
+*/
 void op_pop(stack_t **stack, unsigned int number)
 {
 	stack_t *temp;
-	
+
 	if (*stack == NULL)
 	{
 		fprintf(stderr, "L%i: can't pop an empty stack\n", number);
 		exit(EXIT_FAILURE);
 	}
-
 	temp = *stack;
 	*stack = (*stack)->next;
-	if (*stack)
-		(*stack)->prev = NULL;
 	free(temp);
 }
